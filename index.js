d522b91992ebbf9b2f482c5404d80b7a7317a00e
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-07-10 02:22:37 +0600

    responsive nav and cullup icon in js

---
+++ index.js
@@ -0,0 +1,11 @@
+const icon = document.getElementById("icon");
+const menu = document.getElementById("menu");
+
+icon.addEventListener("click", () => {
+if (menu.className ==="hidden"){
+    menu.classList.remove("hidden")
+}else{
+    menu.classList.add("hidden")
+}
+})
+console.log("hello")
